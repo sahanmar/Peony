627297bdbdfa65e1bce12bd34e8b71e05be9e0e6
Marko Sahan <marko.sahan@gmail.com>
2019-11-09 13:00:40 +0100

    refactored active learning simulation

---
+++ Peony_project/Peony_box/active_learning_simulation/active_learning_simulation.py
@@ -0,0 +1,144 @@
+from PeonyPackage.PeonyDb import MongoDb
+from Peony_box.active_learning_simulation.utils import active_learning_simulation
+from Peony_box.src.transformators.HuffPost_transformator import (
+    HuffPostTransform as transformator,
+)
+from Peony_box.src.transformators.HuffPost_transformator import (
+    HuffPostTransformWordEmbeddings as word_embed_transformator,
+)
+from Peony_database.src.datasets.HuffPost_news_dataset import (
+    COLLECTION_NAME as HuffPost_collection_name,
+    COLLECTION_ID as HuffPost_collection_id,
+)
+from Peony_box.src.acquisition_functions.functions import entropy_sampling
+from Peony_visualization.src.peony_visualization import visualize_two_auc_evolutions
+
+from sklearn.utils import shuffle
+import numpy as np
+
+
+def main():
+
+    api = MongoDb()
+
+    sport_records = api.get_record(
+        collection_name=HuffPost_collection_name,
+        collection_id=HuffPost_collection_id,
+        label="SPORTS",
+        limit=500,
+    )
+
+    comedy_records = api.get_record(
+        collection_name=HuffPost_collection_name,
+        collection_id=HuffPost_collection_id,
+        label="COMEDY",
+        limit=500,
+    )
+
+    # Define model specifications
+    model_1 = "feed_forward_nn_fast_text_embeddings"
+    model_2 = "feed_forward_nn_fast_text_embeddings"
+    algorithm = "nn"
+    acquisition_function_1 = "random"
+    acquisition_function_2 = "entropy"
+    active_learning_loops = 10
+    active_learning_step = 1
+    max_active_learning_iters = 2
+    initial_training_data_size = 10
+    validation_data_size = 1000
+    category_1 = "SPORTS"
+    category_2 = "COMEDY"
+    transformation_needed = False
+
+    instances = sport_records + comedy_records
+    labels = [sample["record"]["label"] for sample in sport_records + comedy_records]
+
+    instances_from_db, labels_from_db = shuffle(instances, labels, random_state=0)
+
+    # HuffPostTransform = word_embed_transformator()
+    HuffPostTransform = transformator()
+    HuffPostTransform.fit(instances_from_db)
+
+    if transformation_needed:
+        instances = instances_from_db
+        labels = labels_from_db
+    else:
+        try:
+            instances = np.asarray(
+                HuffPostTransform.transform_instances(instances_from_db).todense()
+            )
+        except:
+            instances = np.asarray(
+                HuffPostTransform.transform_instances(instances_from_db)
+            )
+        labels = np.asarray(HuffPostTransform.transform_labels(labels_from_db))
+
+    # Get AUC results from an active learning simulation
+    auc_active_learning_random_10_runs_nn = active_learning_simulation(
+        HuffPostTransform,
+        None,
+        active_learning_loops,
+        max_active_learning_iters,
+        active_learning_step,
+        algorithm,
+        instances,
+        labels,
+        initial_training_data_size,
+        transformation_needed,
+    )
+
+    # Pack specifications and resutls to the list for uploading to Peony Database
+    list_to_upload = [
+        model_1,
+        acquisition_function_1,
+        active_learning_loops,
+        active_learning_step,
+        max_active_learning_iters,
+        initial_training_data_size,
+        validation_data_size,
+        category_1,
+        category_2,
+        auc_active_learning_random_10_runs_nn,
+    ]
+
+    # Upload results to Peony Database
+    api.load_model_results(*list_to_upload)
+
+    # Get AUC results from an active learning simulation
+    auc_active_learning_entropy_10_runs_nn = active_learning_simulation(
+        HuffPostTransform,
+        entropy_sampling,
+        active_learning_loops,
+        max_active_learning_iters,
+        active_learning_step,
+        algorithm,
+        instances,
+        labels,
+        initial_training_data_size,
+        transformation_needed,
+    )
+
+    # Pack specifications and resutls to the list for uploading to Peony Database
+    list_to_upload = [
+        model_2,
+        acquisition_function_2,
+        active_learning_loops,
+        active_learning_step,
+        max_active_learning_iters,
+        initial_training_data_size,
+        validation_data_size,
+        category_1,
+        category_2,
+        auc_active_learning_entropy_10_runs_nn,
+    ]
+
+    # Upload results to Peony Database
+    api.load_model_results(*list_to_upload)
+
+    visualize_two_auc_evolutions(
+        auc_active_learning_random_10_runs_nn, auc_active_learning_entropy_10_runs_nn
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ Peony_project/Peony_box/active_learning_simulation/utils.py
@@ -0,0 +1,159 @@
+from Peony_box.src.peony_box_model import PeonyBoxModel
+
+from Peony_box.src.utils import k_fold_corss_validation, auc_metrics
+
+import numpy as np
+import multiprocessing as mp
+import argparse
+
+from IPython.utils import io
+from tqdm import tqdm
+
+
+def reset_validation_data(testing_instances, testing_labels, new_training_indices):
+    new_training_indices = new_training_indices.tolist()
+    training_instances = [testing_instances[index] for index in new_training_indices]
+    training_labels = [testing_labels[index] for index in new_training_indices]
+    testing_instances = np.delete(testing_instances, new_training_indices, axis=0)
+    testing_labels = np.delete(testing_labels, new_training_indices, axis=None)
+
+    return training_instances, training_labels, testing_instances, testing_labels
+
+
+def active_learning_simulation(
+    transformator,
+    acquisition_function,
+    active_learning_loops,
+    max_active_learning_iters,
+    active_learning_step,
+    model,
+    instances,
+    labels,
+    initial_training_data_size,
+    transformation_needed,
+):
+
+    # pool = mp.Pool(mp.cpu_count())
+
+    # Repeat experiment for statistical validation
+    # Return auc results for all runs and different active learning iteration
+
+    # result = [pool.apply(active_learning_simulation_round, args = (transformator, acquisition_function, max_active_learning_iters, active_learning_step, model)) for _ in range(active_learning_loops)]
+
+    # pool.close()
+
+    return [
+        active_learning_simulation_round(
+            transformator,
+            acquisition_function,
+            max_active_learning_iters,
+            active_learning_step,
+            model,
+            instances,
+            labels,
+            initial_training_data_size,
+            transformation_needed,
+        )
+        for _ in tqdm(range(active_learning_loops))
+    ]
+
+
+def active_learning_simulation_round(
+    transformator,
+    acquisition_function,
+    max_active_learning_iters,
+    active_learning_step,
+    model,
+    instances,
+    labels,
+    initial_training_data_size,
+    transformation_needed,
+):
+    auc_active_learning_runs = []
+    # Data preparation
+    auc_active_learning = []
+
+    training_instances = instances[:initial_training_data_size]
+    training_labels = labels[:initial_training_data_size]
+
+    testing_instances = instances[initial_training_data_size:]
+    testing_labels = labels[initial_training_data_size:]
+
+    # Active Learning Pipeline Run
+    peony_model = PeonyBoxModel(
+        transformator,
+        active_learning_step=active_learning_step,
+        acquisition_function=acquisition_function,
+    )
+    with io.capture_output() as captured:  # suppressing output
+        # Fit model with very little set of training data
+        if model == "svm":
+            peony_model.svm_model.fit(
+                training_instances, training_labels, transformation_needed
+            )
+        elif model == "nn":
+            peony_model.feed_forward_nn.fit(
+                training_instances, training_labels, transformation_needed
+            )
+        else:
+            peony_model.random_forest_model.fit(
+                training_instances, training_labels, transformation_needed
+            )
+
+    # Start active learning loop
+    for index in range(max_active_learning_iters):
+        with io.capture_output() as captured:  # suppressing output
+
+            # predict the dataset complement for choosing next training data
+            if model == "svm":
+                predicted = peony_model.svm_model.predict(
+                    testing_instances, transformation_needed
+                )
+            elif model == "nn":
+                predicted = peony_model.feed_forward_nn.predict(
+                    testing_instances, transformation_needed
+                )
+            else:
+                predicted = peony_model.random_forest_model.predict(
+                    testing_instances, transformation_needed
+                )
+
+            labels_for_auc = list(testing_labels[:])
+            auc_active_learning.append(
+                auc_metrics([{"true": labels_for_auc, "predicted": predicted}])
+            )
+
+            # Get indices based on acquisition function
+            if model == "svm":
+                indices = peony_model.svm_model.get_learning_samples(
+                    testing_instances, transformation_needed
+                )
+            elif model == "nn":
+                indices = peony_model.feed_forward_nn.get_learning_samples(
+                    testing_instances, transformation_needed
+                )
+            else:
+                indices = peony_model.random_forest_model.get_learning_samples(
+                    testing_instances, transformation_needed
+                )
+
+            # Reset validation dataset (add training data, remove testing data)
+            training_instances, training_labels, testing_instances, testing_labels = reset_validation_data(
+                testing_instances, testing_labels, indices
+            )
+
+            # Add new learning samples to the model and retrain
+            if model == "svm":
+                peony_model.svm_model.add_new_learning_samples(
+                    training_instances, training_labels, transformation_needed
+                )
+            elif model == "nn":
+                peony_model.feed_forward_nn.add_new_learning_samples(
+                    training_instances, training_labels, transformation_needed
+                )
+            else:
+                peony_model.random_forest_model.add_new_learning_samples(
+                    training_instances, training_labels, transformation_needed
+                )
+
+    return auc_active_learning
