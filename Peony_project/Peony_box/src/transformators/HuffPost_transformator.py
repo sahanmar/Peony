cbdb39e548eb722d957cb0a3bdf05d611713e775
Marko Sahan <marko.sahan@gmail.com>
2020-10-18 15:17:24 +0200

    laser encoder added

--- Peony_project/Peony_box/src/transformators/HuffPost_transformator.py
+++ Peony_project/Peony_box/src/transformators/HuffPost_transformator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import requests
 
 from PeonyPackage.PeonyDb import MongoDb
 from Peony_box.src.transformators.generalized_transformator import Transformator
@@ -46,7 +47,7 @@ class HuffPostTransform(Transformator):
             self.dict_length = len(self.vectorizer.get_feature_names())
 
     def transform_instances(self, data: List[Dict[str, Any]]) -> np.ndarray:
-        transformed_data = [self._transform_text(sample) for sample in tqdm(data)]
+        transformed_data = [_transform_text(sample) for sample in tqdm(data)]
         counts = self.vectorizer.transform(transformed_data)
         return self.transformer.transform(counts)
 
@@ -59,14 +60,6 @@ class HuffPostTransform(Transformator):
         self.vectorizer = CountVectorizer()
         self.fitted = False
         self.dict_length = 0
-
-    @staticmethod
-    def _transform_text(sample: Dict[str, Any]) -> str:
-
-        text = " ".join(
-            [sample["record"]["text"]["title"], sample["record"]["text"]["body"]]
-        )
-        return text
 
     def transform_label(self, sample: str) -> int:
         return self.encoding_mapper[sample]
@@ -83,13 +76,12 @@ class HuffPostTransformWordEmbeddings(Transformator):
         self.api = MongoDb()
         self.encoding_mapper: Dict[str, int] = {}
         self.reverse_mapper: Dict[int, str] = {}
+        self.dim = 300
 
     def fit(self, instances: List[Dict[str, Any]], labels: List[str]) -> None:
         if self.fitted is False:
             print("transforming data...")
-            transformed_data = [
-                self._transform_text(sample) for sample in tqdm(instances)
-            ]
+            transformed_data = [_transform_text(sample) for sample in tqdm(instances)]
             tokenized_text = [
                 token
                 for text in transformed_data
@@ -111,15 +103,6 @@ class HuffPostTransformWordEmbeddings(Transformator):
             self.fitted = True
             self.dict_length = len(self.transformer.keys())
 
-    @staticmethod
-    def _normalize(embedding: Union[np.ndarray, List[float]]) -> np.ndarray:
-        epsilon = 0.1
-        norm = np.linalg.norm(np.asarray(embedding))
-        if norm <= epsilon:
-            return np.asarray([0.0 for i in range(300)])
-        else:
-            return np.asarray(embedding) / norm
-
     def get_embedding_from_database(self, token: str) -> Optional[np.ndarray]:
         embedding = self.api.get_record(
             collection_name="Fasttext_pretrained_embeddings",
@@ -129,13 +112,13 @@ class HuffPostTransformWordEmbeddings(Transformator):
         if embedding is None:
             return [0.0 for i in range(300)]
         else:
-            return self._normalize(embedding["record"]["value"])
+            return _normalize(embedding["record"]["value"], self.dim)
 
     def transform_instances(self, data: List[Dict[str, Any]]) -> np.ndarray:
-        transformed_data = [self._transform_text(sample) for sample in tqdm(data)]
+        transformed_data = [_transform_text(sample) for sample in tqdm(data)]
         return np.asmatrix(
             [
-                self._normalize(
+                _normalize(
                     np.sum(
                         [
                             self.transformer[token]
@@ -143,7 +126,8 @@ class HuffPostTransformWordEmbeddings(Transformator):
                             if token in self.transformer
                         ],
                         axis=0,
-                    )
+                    ),
+                    self.dim,
                 ).tolist()
                 for text in transformed_data
             ]
@@ -158,16 +142,84 @@ class HuffPostTransformWordEmbeddings(Transformator):
         self.fitted = False
         self.dict_length = 0
 
-    @staticmethod
-    def _transform_text(sample: Dict[str, Any]) -> str:
+    def transform_label(self, sample: str) -> int:
+        return self.encoding_mapper[sample]
 
-        text = " ".join(
-            [sample["record"]["text"]["title"], sample["record"]["text"]["body"]]
+    def transform_to_label(self, value: int) -> str:
+        return self.reverse_mapper[value]
+
+
+class HuffPostTransformLaserWordEmbeddings(Transformator):
+
+    """
+    Before you start use this transformer visit this page
+    https://github.com/facebookresearch/LASER/tree/master/docker
+    and run laser in docker. Use port 59012
+    """
+
+    def __init__(self):
+        self.transformer = lambda text: requests.get(
+            url=self.url, params={"q": text, "lang": "en"}
+        ).json()["embedding"]
+        self.fitted: bool = False
+        self.dim = 1024
+        self.url = "http://127.0.0.1:59012/vectorize"
+        self.encoding_mapper: Dict[str, int] = {}
+        self.reverse_mapper: Dict[int, str] = {}
+
+    def fit(self, labels: List[str]) -> None:
+        if self.fitted is False:
+            print("laser encoder is encoding on-prem...")
+            print("creating labels encoding hash map...")
+            self.encoding_mapper = {
+                value: index for index, value in enumerate(set(labels))
+            }
+            self.reverse_mapper = {
+                index: value for index, value in enumerate(set(labels))
+            }
+            self.fitted = True
+
+    def transform_instances(self, data: List[Dict[str, Any]]) -> np.ndarray:
+        transformed_data = [_transform_text(sample) for sample in data]
+        return np.asmatrix(
+            [
+                _normalize(
+                    np.sum(
+                        self.transformer(text),
+                        axis=0,
+                    ),
+                    self.dim,
+                ).tolist()
+                for text in tqdm(transformed_data)
+            ]
         )
-        return text
+
+    def transform_labels(self, data: List[str]) -> np.ndarray:
+        transformed_data = [self.transform_label(sample) for sample in tqdm(data)]
+        return np.asarray(transformed_data).ravel()
+
+    def reset(self) -> None:
+        self.fitted = False
 
     def transform_label(self, sample: str) -> int:
         return self.encoding_mapper[sample]
 
     def transform_to_label(self, value: int) -> str:
         return self.reverse_mapper[value]
+
+
+def _normalize(embedding: Union[np.ndarray, List[float]], dim: int) -> np.ndarray:
+    epsilon = 0.1
+    norm = np.linalg.norm(np.asarray(embedding))
+    if norm <= epsilon:
+        return np.asarray([0.0 for i in range(dim)])
+    else:
+        return np.asarray(embedding) / norm
+
+
+def _transform_text(sample: Dict[str, Any]) -> str:
+
+    text = " ".join(
+        [sample["record"]["text"]["title"], sample["record"]["text"]["body"]]
+    )
+    return text
